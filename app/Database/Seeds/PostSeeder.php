bfbf26fe48913c2d4fd27f6c5dea9d0e1255b1fb
Dang Nguyen <nguyenlongdang04121999@gmail.com>
2021-07-12 14:46:57 +0700

    Truncate data

--- app/Database/Seeds/PostSeeder.php
+++ app/Database/Seeds/PostSeeder.php
@@ -5,21 +5,19 @@ namespace App\Database\Seeds;
 use CodeIgniter\Database\Seeder;
 use CodeIgniter\I18n\Time;
 use App\Models\Post;
-use App\Libraries\Slug;
 
 class PostSeeder extends Seeder
 {
 	public function run()
 	{
 		$post = new Post;
-		$slug = new Slug;
 		$faker = \Faker\Factory::create();
 
 		for ($i = 0; $i < 50; $i++) {
 			$post->save(
 				[
-					'name' 		       	=>    $faker->name,
-					'slug'       		=>    $slug->str_slug($faker->name),
+					'name' 		       	=>    $faker->name(),
+					'slug'       		=>    $faker->slug(),
 					'cat_id'    		=>    rand(1, 10),
 					'is_type'    		=>    rand(0, 4),
 					'province_id'		=>	  rand(1, 96),
@@ -27,9 +25,10 @@ class PostSeeder extends Seeder
 					'user_id' 			=>	  rand(1, 63),
 					'price'				=> 	  rand(),
 					'contact_address'   =>    $faker->text(),
-					'video'				=> 	  'https://www.youtube.com/watch?v=dgzOTiZyb9U',
+					'description'		=>	  $faker->randomHtml(),
+					'video'				=> 	  'https://www.youtube.com/watch?v=3ufQGXG9aNY',
 					'video_description' =>    $faker->text(),
-					'thumb_list' 		=> 	  \Faker\Provider\Image::imageUrl(640, 480),
+					'thumb_list' 		=> 	  \Faker\Provider\Image::imageUrl(350, 250),
 					'expire_from'		=>	  '2021-07-04',
 					'expire_to'			=>	  '2021-08-03',
 					'status'			=>    STATUS_POST_ACTIVE,
